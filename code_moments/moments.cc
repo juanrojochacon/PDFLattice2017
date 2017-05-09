51800e926ca41c52573b5d7f1fcfc90ffa7a7df6
Juan Rojo <juan.rojo@cern.ch>
2017-05-09 09:26:04 +0200

    Code to compute moments added

---
+++ code_moments/moments.cc
@@ -0,0 +1,141 @@
+
+#include "momentsint.h"
+
+int main(int argc, char **argv)
+{  
+  cout << "\n ****************************************\n";
+  cout <<   " * Computation of PDF moments *\n";
+  cout <<   " ****************************************\n\n";
+  cout << endl;
+
+  //! Relative error integration
+  double const eps = 1e-6;
+
+  // Max number of PDF error members
+  int const nrepmax=101;
+
+  // Scale at which moments are evaluated
+  double const Q = 5; 
+
+  LHAPDF::setVerbosity(0);
+  MomIntegral *lum = new MomIntegral(eps);
+
+  // Array of PDF sets
+  vector<string> pdfname;
+
+  // Type of PDF errors
+  vector<string> pdfertype;
+
+
+  ////////////////////////////////////
+  // List of PDF sets for which we
+  // want to compute moments
+  
+  // NNPDF3.1 NNLO
+  pdfname.push_back("NNPDF31_nnlo_as_0118");
+  // This is a Monte Carlo set
+  pdfertype.push_back("mc");
+
+  // CT14NNLO
+  pdfname.push_back("CT14NNLO");
+  // This is a Hessian set
+  pdfertype.push_back("hessianCT");
+
+  // MMHT2014NNLO
+  pdfname.push_back("MMHT2014nnlo68cl");
+  // This is also a Hessian set
+  pdfertype.push_back("hessian");
+  
+  // ABMP16 NNLO
+  pdfname.push_back("ABMP16_5_nnlo");
+  // This is a symmetric Hessian set
+  pdfertype.push_back("symhessian");
+
+  double cv_ref=0;
+
+  for (size_t iPdf = 0; iPdf < pdfname.size(); iPdf++){
+
+    // Initialize PDF set
+    LHAPDF::initPDFSet(pdfname[iPdf]);
+
+    // Get number of replicas
+    int repfinal = LHAPDF::numberPDF();
+
+      // Begin loop over replicas
+      double pdfmom[nrepmax]={0.0};
+      for (int irep = 0; irep <= repfinal; irep++){
+	
+	// Initialize replica
+	LHAPDF::initPDF(irep);
+	
+	// Get the momentum fraction
+	pdfmom[irep] = lum->getMom(Q);
+		
+      } // end loop over replicas
+
+      double cv=0;
+      double err=0;
+
+      
+      if(pdfertype[iPdf]=="mc"){
+	// Now compute 68% CL
+	
+	vector<double> values;
+	for (int nrep = 1; nrep <= repfinal; nrep++)
+	  {
+	    values.push_back(pdfmom[nrep]);
+	  }
+	// Now sort the array
+	sort(values.begin(),values.end());
+	
+	// Compute the central value as the midpoint of the 68% CL interval
+	int rep_min=repfinal*0.16;
+	int rep_max=repfinal*0.84;
+	cv= 0.5*(values.at(rep_min)+values.at(rep_max));
+	err= 0.5*(values.at(rep_max)-values.at(rep_min));
+	cv_ref=cv;
+
+      }
+      
+      if(pdfertype[iPdf]=="hessianCT"){
+	cv=pdfmom[0];
+	for (int nrep = 1; nrep <= repfinal/2; nrep++)
+	  {
+	    err += pow(pdfmom[2*nrep-1]-pdfmom[2*nrep],2.0 );
+	  }
+	err=0.5 *pow(err,0.5)/1.642;
+
+      }
+
+      if(pdfertype[iPdf]=="hessian"){
+	cv=pdfmom[0];
+	for (int nrep = 1; nrep <= repfinal/2; nrep++)
+	  {
+	    err += pow(pdfmom[2*nrep-1]-pdfmom[2*nrep],2.0 );
+	  }
+	err=0.5 *pow(err,0.5);
+
+      }
+
+      if(pdfertype[iPdf]=="symhessian"){
+	cv=pdfmom[0];
+	for (int nrep = 1; nrep <= repfinal; nrep++)
+	  {
+	    err += pow(pdfmom[nrep]-pdfmom[0],2.0 );
+	  }
+	err=pow(err,0.5);
+
+      }
+	
+      cout<<"pdfset = "<<pdfname[iPdf]<<endl;
+      
+      cout<<" <x(u-d)> = "<<cv<<" +- "<<err<<" ( "<< 100*err/cv  <<"% )  (shift from cv ref = "<<
+	100*(cv-cv_ref)/cv_ref<<"% )"<<endl;
+      
+      
+  } // End loop over PDF sets
+  
+  
+  return 0;
+}
+
---
+++ code_moments/momentsint.h
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <sstream>
+
+#include "utils.h"
+#include "LHAPDF/LHAPDF.h"
+
+#include "TCanvas.h"
+#include "TGraphErrors.h"
+#include "TGraphAsymmErrors.h"
+#include "TAxis.h"
+#include "TH1F.h"
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TString.h"
+#include "TLegend.h"
+#include "TPaveStats.h"
+#include "TMultiGraph.h"
+
+double max ( const double a, const double b ) {
+  return (a<b)?b:a;
+}
+
+class MomIntegral
+{
+ private:
+  double Q;
+  double xmin;
+  double eps;
+  
+ public:
+  MomIntegral(double err = 1e-12) { eps = err;}
+
+  // Here one choose the argument of the integral over x
+  // That is the type of moment that is being computed
+  double momwrap(double x)
+  {
+    // x*(u - d)
+    double xpdf = ( LHAPDF::xfx(x, Q, 2) + LHAPDF::xfx(x, Q, -1) );
+    
+    return xpdf;
+  }
+  
+  double getMom(double Q)
+  {
+    this->Q = Q;
+    xmin=1e-7;
+    double result = dgauss(xmin, 1.0, eps);
+    return result;
+  }
+  
+  double dgauss(double a, double b, double EPS){
+    // FROM ROOT
+    //  Return Integral of function between a and b.
+    
+    const double kHF = 0.5;
+    const double kCST = 5./1000;
+    
+    double x[12] = { 0.96028985649753623,  0.79666647741362674,
+		     0.52553240991632899,  0.18343464249564980,
+		     0.98940093499164993,  0.94457502307323258,
+		     0.86563120238783174,  0.75540440835500303,
+		     0.61787624440264375,  0.45801677765722739,
+		     0.28160355077925891,  0.09501250983763744};
+    
+    double w[12] = { 0.10122853629037626,  0.22238103445337447,
+		     0.31370664587788729,  0.36268378337836198,
+		     0.02715245941175409,  0.06225352393864789,
+		     0.09515851168249278,  0.12462897125553387,
+		     0.14959598881657673,  0.16915651939500254,
+		     0.18260341504492359,  0.18945061045506850};
+    
+    double h, aconst, bb, aa, c1, c2, u, s8, s16, f1, f2;
+    double xx[1];
+    int i;
+    
+    h = 0;
+    if (b == a) return h;
+    aconst = kCST/std::abs(b-a);
+    bb = a;
+  CASE1:
+    aa = bb;
+    bb = b;
+  CASE2:
+    c1 = kHF*(bb+aa);
+    c2 = kHF*(bb-aa);
+    s8 = 0;
+    for (i=0;i<4;i++) {
+      u     = c2*x[i];
+      xx[0] = c1+u;
+      f1    = momwrap(xx[0]);
+      xx[0] = c1-u;
+      f2    = momwrap(xx[0]);
+      s8   += w[i]*(f1 + f2);
+    }
+    s16 = 0;
+    for (i=4;i<12;i++) {
+      u     = c2*x[i];
+      xx[0] = c1+u;
+      f1    = momwrap(xx[0]);
+      xx[0] = c1-u;
+      f2    = momwrap(xx[0]);
+      s16  += w[i]*(f1 + f2);
+    }
+    s16 = c2*s16;
+    if (std::abs(s16-c2*s8) <= EPS*(1. + std::abs(s16))) {
+      h += s16;
+      if(bb != b) goto CASE1;
+    } else {
+      bb = c1;
+      if(1. + aconst*std::abs(c2) != 1) goto CASE2;
+      h = s8;  //this is a crude approximation (cernlib function returned 0 !)
+    }
+    
+    return h;
+  }
+};
+
+
